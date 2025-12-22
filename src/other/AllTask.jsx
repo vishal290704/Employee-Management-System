d42d7668b62a80bf41e8a2adadf9bd3d91ff5a45
Vishal Srivastava <vsjee2002@gmail.com>
2025-12-22 11:33:35 +0530

    Fixing color of div in AllTasks

--- src/other/AllTask.jsx
+++ src/other/AllTask.jsx
@@ -7,23 +7,25 @@ const AllTask = () => {
   console.log(authData.employees);
   
   return (
-    <div className='bg-[#1c1c1c] p-5 mt-10 rounded h-48 overflow-auto'>
+    <div className='bg-[#1c1c1c] p-5 mt-10 rounded h-48'>
       <div className='bg-red-700 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2 className='w-1/5 bg-red-400'>Employee Name</h2>
-            <h3 className='w-1/5 bg-red-400'>New Task</h3>
-            <h5 className='w-1/5 bg-red-400'>Active Task</h5>
-            <h5 className='w-1/5 bg-red-400'>Completed Task</h5>
-            <h5 className='w-1/5 bg-red-400'>Failed Task</h5>
+            <h2 className='w-1/5 '>Employee Name</h2>
+            <h3 className='w-1/5 '>New Task</h3>
+            <h5 className='w-1/5 '>Active Task</h5>
+            <h5 className='w-1/5 '>Completed Task</h5>
+            <h5 className='w-1/5 '>Failed Task</h5>
         </div>
-      {authData.employees.map(function(elem){
-        return  <div className='bg-red-700 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2 className='w-1/5 bg-red-400'>{elem.firstName}</h2>
-            <h3 className='w-1/5 bg-red-400'>Task</h3>
-            <h5 className='w-1/5 bg-red-400'>Status</h5>
-            <h5 className='w-1/5 bg-red-400'>Status</h5>
-            <h5 className='w-1/5 bg-red-400'>Status</h5>
+    <div className='h-[80%] overflow-auto'>
+        {authData.employees.map(function(elem){
+        return  <div className='bg-gray-800 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2 className='w-1/5'>{elem.firstName}</h2>
+            <h3 className='w-1/5 text-blue-400'>Task</h3>
+            <h5 className='w-1/5 text-yellow-400'>Status</h5>
+            <h5 className='w-1/5 text-green-700'>Status</h5>
+            <h5 className='w-1/5 text-red-800'>Status</h5>
         </div>
       })}
+    </div>
        
          {/* <div className='bg-blue-700 mb-2 py-2 px-4 flex justify-between rounded'>
             <h2>Vishal</h2>
