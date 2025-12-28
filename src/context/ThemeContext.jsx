109936af19a00e3844e735ed904563ee4c65b99a
Vishal Srivastava <vsjee2002@gmail.com>
2025-12-28 14:03:38 +0530

    ThemeContext file created for dark/light mode

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,13 @@
+import React, { createContext } from 'react'
+
+export const ThemeContext = createContext()
+
+const ThemeContext = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ThemeContext
